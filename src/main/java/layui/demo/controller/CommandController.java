63e69e2918b6e3f447847be29a059138630c4b54
zl <zhaole@act.buaa.edu.cn>
2019-10-14 06:37:53 -0700

    set conf file

--- src/main/java/layui/demo/controller/CommandController.java
+++ src/main/java/layui/demo/controller/CommandController.java
@@ -13,20 +13,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.ByteArrayOutputStream;
+import java.io.*;
 
 @Controller
 //@RequestMapping("/cmd")
 public class CommandController
 {
     private static final Logger logger = LoggerFactory.getLogger(CommandController.class);
-
+    //String path = "/data/zhaole/causaltest/causalwebserver/src/main/resources/conf.properties";
+    String path="/home/zl/Documents/test/conf.properties";
     @GetMapping("/pwd")
     //@ResponseBody
     public String pwd(Model model)
     {
         try {
-            String command = "pwd";
+            String command = "vim ";
             //接收正常结果流
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             //接收异常结果流
@@ -57,6 +58,89 @@ public class CommandController
                         @RequestParam("consistency") String consistency)
     {
         logger.info(website+","+count+","+consistency);
+        setConfig(website,count,consistency);
+        logger.info("set conf file completed.");
         return "hbase";
     }
+
+    public void setConfig(String website,String count,String consistency)
+    {
+        BufferedReader br = null;
+        String line = null;
+        StringBuffer bufAll = new StringBuffer();  //保存修改过后的所有内容，不断增加
+
+        try {
+                br = new BufferedReader(new FileReader(path));
+                while ((line = br.readLine()) != null)
+                {
+                    StringBuffer buf = new StringBuffer();
+                    if (line.startsWith("operationcount="))
+                    {
+                        buf.append(line);
+                        buf.replace(15,line.length(),count);
+                        buf.append(System.getProperty("line.separator"));
+                        bufAll.append(buf);
+                    }
+                    else if (line.startsWith("database="))
+                    {
+                        buf.append(line);
+                        buf.replace(9,line.length(),"Hbase");
+                        buf.append(System.getProperty("line.separator"));
+                        bufAll.append(buf);
+                    }
+                    else if (line.startsWith("website="))
+                    {
+                        buf.append(line);
+                        buf.replace(8,line.length(),website);
+                        buf.append(System.getProperty("line.separator"));
+                        bufAll.append(buf);
+                    }
+                    else if (line.startsWith("consistency="))
+                    {
+                        buf.append(line);
+                        buf.replace(12,line.length(),consistency);
+                        buf.append(System.getProperty("line.separator"));
+                        bufAll.append(buf);
+                    }else{
+                        buf.append(line);
+                        buf.append(System.getProperty("line.separator"));
+                        bufAll.append(buf);
+                    }
+                }
+        } catch (Exception e)
+        {
+                e.printStackTrace();
+        } finally
+        {
+            if (br != null)
+            {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    br = null;
+                }
+            }
+        }
+        BufferedWriter bw = null;
+        try
+        {
+            bw = new BufferedWriter(new FileWriter(path));
+            bw.write(bufAll.toString());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(bw != null)
+            {
+                try {
+                    bw.close();
+                }catch (IOException e){
+                    bw = null;
+                }
+            }
+        }
+
+
+    }
+
 }
